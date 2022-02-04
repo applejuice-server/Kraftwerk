8539df40d9fb905bc2b367125d2d41621d37376b
Juan <jpichardo369@gmail.com>
2022-02-04 16:07:55 -0500

    Make the arena send you back
    Signed-off-by: Juan <jpichardo369@gmail.com>

--- src/main/kotlin/pink/mino/kraftwerk/commands/ArenaCommand.kt
+++ src/main/kotlin/pink/mino/kraftwerk/commands/ArenaCommand.kt
@@ -5,6 +5,7 @@ import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
 import pink.mino.kraftwerk.features.ArenaFeature
+import pink.mino.kraftwerk.features.SpawnFeature
 import pink.mino.kraftwerk.utils.Chat
 import pink.mino.kraftwerk.utils.GameState
 
@@ -21,7 +22,8 @@ class ArenaCommand : CommandExecutor {
         }
         if (GameState.currentState == GameState.LOBBY) {
             if (sender.world.name == "Arena") {
-                Chat.sendMessage(sender, "&cYou can't use this command in the arena.")
+                SpawnFeature.instance.send(sender)
+                sender.sendMessage(Chat.colored("${Chat.prefix} &7You left the arena."))
                 return false
             }
             ArenaFeature.instance.send(sender)
