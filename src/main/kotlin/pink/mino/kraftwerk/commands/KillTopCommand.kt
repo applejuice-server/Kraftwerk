5cebe6c3857949a535da1099d117ee52736a9834
Juan <jpichardo369@gmail.com>
2022-02-11 21:55:29 -0500

    i forgor to commit the actual killtop command
    Signed-off-by: Juan <jpichardo369@gmail.com>

---
+++ src/main/kotlin/pink/mino/kraftwerk/commands/KillTopCommand.kt
@@ -0,0 +1,44 @@
+package pink.mino.kraftwerk.commands
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import pink.mino.kraftwerk.features.SettingsFeature
+import pink.mino.kraftwerk.utils.Chat
+import pink.mino.kraftwerk.utils.GameState
+import java.util.*
+
+
+class KillTopCommand : CommandExecutor {
+    override fun onCommand(
+        sender: CommandSender,
+        command: Command?,
+        label: String?,
+        args: Array<String>
+    ): Boolean {
+        if (GameState.currentState != GameState.INGAME) {
+            Chat.sendMessage(sender, "&cYou can't use this command right now.")
+            return false
+        }
+        if (SettingsFeature.instance.data!!.get("game.kills") == null) {
+            Chat.sendMessage(sender, "&cThere are no kills yet.")
+            return false
+        }
+        val map = LinkedHashMap<String, Int>()
+        for (key in SettingsFeature.instance.data!!.getConfigurationSection("game.kills").getKeys(false)) {
+            map[key] = SettingsFeature.instance.data!!.getInt("game.kills.${key}")
+        }
+        val entries: List<Map.Entry<String, Int>> = ArrayList<Map.Entry<String, Int>>(map.entries)
+        Collections.sort(entries
+        ) { o1, o2 -> o1!!.value - o2!!.value }
+        Collections.reverse(entries)
+        Chat.sendMessage(sender, Chat.line)
+        Chat.sendCenteredMessage(sender, "&c&lKill Leaderboard")
+        Chat.sendMessage(sender , " ")
+        for ((key, value) in entries) {
+            Chat.sendCenteredMessage(sender, "&7${key} ${Chat.dash} &f${value}")
+        }
+        Chat.sendMessage(sender, Chat.line)
+        return true
+    }
+}
