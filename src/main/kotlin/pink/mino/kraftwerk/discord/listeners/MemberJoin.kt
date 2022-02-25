95dd0b9e07ff3c7becbef47580145a955688afbb
Juan <jpichardo369@gmail.com>
2022-02-25 00:49:22 -0500

    Fix quotes appearing in the join message
    Signed-off-by: Juan <jpichardo369@gmail.com>

--- src/main/kotlin/pink/mino/kraftwerk/discord/listeners/MemberJoin.kt
+++ src/main/kotlin/pink/mino/kraftwerk/discord/listeners/MemberJoin.kt
@@ -8,7 +8,7 @@ class MemberJoin : ListenerAdapter() {
     override fun onGuildMemberJoin(e: GuildMemberJoinEvent) {
         val member = e.member
         val guild = e.guild
-        Discord.instance!!.getTextChannelById(757930533505335348)!!.sendMessage("\"Welcome ${member.asMention} to the :beverage_box: **applejuice Discord**!\"").queue()
+        Discord.instance!!.getTextChannelById(757930533505335348)!!.sendMessage("Welcome ${member.asMention} to the :beverage_box: **applejuice Discord**!").queue()
         guild.getRoleById(793406242013839381)?.let { guild.addRoleToMember(member.id, it).queue() }
         guild.getRoleById(761262435956162571)?.let { guild.addRoleToMember(member.id, it).queue() }
     }
