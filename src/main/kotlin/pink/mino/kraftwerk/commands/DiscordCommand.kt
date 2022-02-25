264c84089fefba6c4a18e138baaf50c759a88fa2
Juan <jpichardo369@gmail.com>
2022-02-24 22:14:19 -0500

    Add discord link
    Signed-off-by: Juan <jpichardo369@gmail.com>

---
+++ src/main/kotlin/pink/mino/kraftwerk/commands/DiscordCommand.kt
@@ -0,0 +1,18 @@
+package pink.mino.kraftwerk.commands
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import pink.mino.kraftwerk.utils.Chat
+
+class DiscordCommand : CommandExecutor {
+    override fun onCommand(
+        sender: CommandSender,
+        command: Command?,
+        label: String?,
+        args: Array<String>
+    ): Boolean {
+        sender.sendMessage(Chat.colored("${Chat.prefix} The discord URL is: &chttps://discord.gg/X7ujJThANP"))
+        return false
+    }
+}
