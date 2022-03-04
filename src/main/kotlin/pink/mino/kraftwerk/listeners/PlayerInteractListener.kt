cf001d006e7c626a6e951cc1ded70224cb768891
Juan <jpichardo369@gmail.com>
2022-03-03 20:23:11 -0500

    Add sheep string
    Signed-off-by: Juan <jpichardo369@gmail.com>

---
+++ src/main/kotlin/pink/mino/kraftwerk/listeners/PlayerInteractListener.kt
@@ -0,0 +1,19 @@
+package pink.mino.kraftwerk.listeners
+
+import org.bukkit.Material
+import org.bukkit.entity.EntityType
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.player.PlayerInteractAtEntityEvent
+import org.bukkit.inventory.ItemStack
+import java.util.*
+
+class PlayerInteractListener : Listener {
+    @EventHandler
+    fun onPlayerInteractWithEntity(e: PlayerInteractAtEntityEvent) {
+        if (e.player.itemInHand.type != Material.SHEARS) return
+        if (e.rightClicked.type == EntityType.SHEEP) {
+            if (Random().nextInt(100) < 33) e.rightClicked.world.dropItemNaturally(e.rightClicked.location, ItemStack(Material.STRING))
+        }
+    }
+}
