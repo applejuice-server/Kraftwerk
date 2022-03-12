e4877e131a09a13a6c99ae9841465e4f184510b8
Juan <jpichardo369@gmail.com>
2022-03-12 11:42:25 -0500

    Get started on Avengers
    Signed-off-by: Juan <jpichardo369@gmail.com>

---
+++ src/main/kotlin/pink/mino/kraftwerk/scenarios/list/AvengersScenario.kt
@@ -0,0 +1,11 @@
+package pink.mino.kraftwerk.scenarios.list
+
+import org.bukkit.Material
+import pink.mino.kraftwerk.scenarios.Scenario
+
+class AvengersScenario : Scenario(
+    "Avengers",
+    "Players are assigned a random power like an Avenger, you'll be told the effects of said power.",
+    "avengers",
+    Material.DIAMOND_SWORD
+)
