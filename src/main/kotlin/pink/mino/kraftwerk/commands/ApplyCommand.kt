44d264e65538ac232ccc1910633edddd54fb42ff
Juan <jpichardo369@gmail.com>
2022-03-16 23:40:19 -0400

    Add apply command & info-nouncements
    Signed-off-by: Juan <jpichardo369@gmail.com>

---
+++ src/main/kotlin/pink/mino/kraftwerk/commands/ApplyCommand.kt
@@ -0,0 +1,18 @@
+package pink.mino.kraftwerk.commands
+
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import pink.mino.kraftwerk.utils.Chat
+
+class ApplyCommand : CommandExecutor {
+    override fun onCommand(
+        sender: CommandSender,
+        command: Command?,
+        label: String?,
+        args: Array<String>
+    ): Boolean {
+        sender.sendMessage(Chat.colored("${Chat.prefix} The staff application URL is: &chttps://forms.gle/Z2fYxzqLLNDd3Fjv7"))
+        return true
+    }
+}
---
+++ src/main/kotlin/pink/mino/kraftwerk/features/InfoFeature.kt
@@ -0,0 +1,25 @@
+package pink.mino.kraftwerk.features
+
+import org.bukkit.Bukkit
+import org.bukkit.scheduler.BukkitRunnable
+import pink.mino.kraftwerk.utils.Chat
+import kotlin.random.Random
+
+class InfoFeature : BukkitRunnable() {
+    val prefix: String = "&8[&4Info&8]&7"
+    private val announcements = listOf(
+        Chat.colored("$prefix Join our discord server using &c/discord&7!"),
+        Chat.colored("$prefix Apply for staff using &c/apply&7!"),
+        Chat.colored("$prefix Like our server @ &cnamemc.com/server/applejuice.bar&7!"),
+        Chat.colored("$prefix This server was custom coded by &fminota&7."),
+        Chat.colored("$prefix Want more games hosted? Apply for staff @ &c/apply&7!"),
+        Chat.colored("$prefix Wanna know when games are hosted & more? Join our discord @ &c/discord&7."),
+        Chat.colored("$prefix Follow us on twitter &b@applejuiceuhc&7!")
+    )
+    override fun run() {
+        if (Bukkit.getOnlinePlayers().isNotEmpty()) {
+            val chosen = announcements[Random.nextInt(announcements.size)]
+            Bukkit.broadcastMessage(chosen)
+        }
+    }
+}
