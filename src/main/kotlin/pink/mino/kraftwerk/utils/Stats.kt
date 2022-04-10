3d274c5f56b5f22c5a61ba12cfedcfcc388a9d2d
Juan <jpichardo369@gmail.com>
2022-04-09 20:10:03 -0400

    Attempt stats again
    Signed-off-by: Juan <jpichardo369@gmail.com>

--- src/main/kotlin/pink/mino/kraftwerk/utils/Stats.kt
+++ src/main/kotlin/pink/mino/kraftwerk/utils/Stats.kt
@@ -73,11 +73,109 @@ class StatsPlayer(val player: Player) : Listener {
     } catch (e: SQLException) {
         e.printStackTrace()
     }
+
+    fun saveAll() {
+        Schedulers.async().run runnable@ {
+            this.save("kills")
+            this.save("wins")
+            this.save("deaths")
+            this.save("games_played")
+            this.save("diamonds_mined")
+            this.save("gold_mined")
+            this.save("iron_mined")
+            this.save("gapples_eaten")
+            this.save("times_crafted")
+            this.save("times_enchanted")
+        }
+    }
+
+    fun save(obj: String) = try {
+        Schedulers.async().run runnable@ {
+            JavaPlugin.getPlugin(Kraftwerk::class.java).dataSource.use { hikari ->
+                val save = hikari.connection.prepareStatement(SAVE)
+                var value = 0
+                when (obj) {
+                    "kills" -> {
+                        value = this.kills
+                    }
+                    "wins" -> {
+                        value = this.wins
+                    }
+                    "deaths" -> {
+                        value = this.deaths
+                    }
+                    "games_played" -> {
+                        value = this.gamesPlayed
+                    }
+                    "diamonds_mined" -> {
+                        value = this.diamondsMined
+                    }
+                    "gold_mined" -> {
+                        value = this.goldMined
+                    }
+                    "iron_mined" -> {
+                        value = this.ironMined
+                    }
+                    "gapples_eaten" -> {
+                        value = this.gapplesEaten
+                    }
+                    "times_crafted" -> {
+                        value = this.timesCrafted
+                    }
+                    "times_enchanted" -> {
+                        value = this.timesEnchanted
+                    }
+                }
+                save.setString(1, obj)
+                save.setString(2, value.toString())
+                save.setString(3, player.uniqueId.toString())
+                save.execute()
+                print("Set $obj for ${player.name} to $value")
+            }
+        }
+    } catch (e: SQLException) {
+        e.printStackTrace()
+    }
+
+    fun add(obj: String, adder: Int = 1) {
+        when (obj) {
+            "kills" -> {
+                this.kills += adder
+            }
+            "wins" -> {
+                this.wins += adder
+            }
+            "deaths" -> {
+                this.deaths += adder
+            }
+            "games_played" -> {
+                this.gamesPlayed += adder
+            }
+            "diamonds_mined" -> {
+                this.diamondsMined += adder
+            }
+            "gold_mined" -> {
+                this.goldMined += adder
+            }
+            "iron_mined" -> {
+                this.ironMined += adder
+            }
+            "gapples_eaten" -> {
+                this.gapplesEaten += adder
+            }
+            "times_crafted" -> {
+                this.timesCrafted += adder
+            }
+            "times_enchanted" -> {
+                this.timesEnchanted += adder
+            }
+        }
+    }
 }
 
 class StatsHandler : Listener {
     companion object {
-        private val statsPlayers: HashMap<UUID, StatsPlayer> = HashMap()
+        val statsPlayers: HashMap<UUID, StatsPlayer> = HashMap()
         fun addStatsPlayer(player: Player) {
             if (statsPlayers[player.uniqueId] == null) {
                 statsPlayers[player.uniqueId] = StatsPlayer(player)
@@ -91,6 +189,7 @@ class StatsHandler : Listener {
                 statsPlayers[player.uniqueId]!!.load("gapples_eaten")
                 statsPlayers[player.uniqueId]!!.load("times_crafted")
                 statsPlayers[player.uniqueId]!!.load("times_enchanted")
+                print("Loaded stats for ${player.name}.")
             }
         }
     }
