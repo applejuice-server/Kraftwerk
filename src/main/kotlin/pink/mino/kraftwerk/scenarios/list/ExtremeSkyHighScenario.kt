57e17ff52603c31a8f8b562df4999a60bb88997a
Juan <jpichardo369@gmail.com>
2022-04-14 14:53:49 -0400

    Added Weakest Link & Extreme SkyHigh
    Signed-off-by: Juan <jpichardo369@gmail.com>

---
+++ src/main/kotlin/pink/mino/kraftwerk/scenarios/list/ExtremeSkyHighScenario.kt
@@ -0,0 +1,75 @@
+package pink.mino.kraftwerk.scenarios.list
+
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
+import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitRunnable
+import pink.mino.kraftwerk.Kraftwerk
+import pink.mino.kraftwerk.features.SpecFeature
+import pink.mino.kraftwerk.scenarios.Scenario
+import pink.mino.kraftwerk.scenarios.ScenarioHandler
+import pink.mino.kraftwerk.utils.Chat
+import pink.mino.kraftwerk.utils.GameState
+
+class ExtremeSkyHighIterator : BukkitRunnable() {
+    var timer = 30
+    override fun run() {
+        timer -= 1
+        if (timer == 0) {
+            for (player in Bukkit.getOnlinePlayers()) {
+                if (player.location.y < 200) {
+                    if (!SpecFeature.instance.getSpecs().contains(player.name)) {
+                        player.damage(4.0)
+                        Chat.sendMessage(player, "&cYou've been damaged for not being above y-101.")
+                    }
+                }
+            }
+            timer = 45
+        }
+        if (!ScenarioHandler.getActiveScenarios().contains(ScenarioHandler.getScenario("extremeskyhigh"))) {
+            cancel()
+        }
+        if (GameState.currentState != GameState.INGAME) {
+            cancel()
+        }
+    }
+}
+
+class ExtremeSkyHighScenario : Scenario(
+    "Extreme SkyHigh",
+    "At PvP, if you're not above y-200, you will take 2 hearts of damage.",
+    "extremeskyhigh",
+    Material.FEATHER
+) {
+    override fun onPvP() {
+        ExtremeSkyHighIterator().runTaskTimer(JavaPlugin.getPlugin(Kraftwerk::class.java), 0L, 20L)
+        Bukkit.broadcastMessage(Chat.colored("${Chat.prefix} The damage tick for &fExtreme SkyHigh&7 has started, the damage tick happen every 45 seconds."))
+    }
+
+    override fun onStart() {
+        val shovel = ItemStack(Material.DIAMOND_SPADE)
+        val meta = shovel.itemMeta
+        meta.spigot().isUnbreakable = true
+        meta.addEnchant(Enchantment.DIG_SPEED, 10, true)
+        shovel.itemMeta = meta
+        for (player in Bukkit.getOnlinePlayers()) {
+            if (!SpecFeature.instance.getSpecs().contains(player.name)) {
+                player.inventory.addItem(ItemStack(Material.STAINED_CLAY, 128, 14), ItemStack(Material.PUMPKIN, 2), ItemStack(Material.SNOW_BLOCK, 4), ItemStack(Material.STRING, 2), shovel, ItemStack(Material.FEATHER, 16))
+                Chat.sendMessage(player, "${Chat.prefix} You've been given your SkyHigh items.")
+            }
+        }
+    }
+
+    override fun givePlayer(player: Player) {
+        val shovel = ItemStack(Material.DIAMOND_SPADE)
+        val meta = shovel.itemMeta
+        meta.spigot().isUnbreakable = true
+        meta.addEnchant(Enchantment.DIG_SPEED, 10, true)
+        shovel.itemMeta = meta
+        player.inventory.addItem(ItemStack(Material.STAINED_CLAY, 128, 14), ItemStack(Material.PUMPKIN, 2), ItemStack(Material.SNOW_BLOCK, 4), ItemStack(Material.STRING, 2), shovel, ItemStack(Material.FEATHER, 16))
+        Chat.sendMessage(player, "${Chat.prefix} You've been given your SkyHigh items.")
+    }
+}
---
+++ src/main/kotlin/pink/mino/kraftwerk/scenarios/list/WeakestLinkScenario.kt
@@ -0,0 +1,74 @@
+package pink.mino.kraftwerk.scenarios.list
+
+import com.google.common.collect.Ordering
+import org.bukkit.Bukkit
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitRunnable
+import pink.mino.kraftwerk.Kraftwerk
+import pink.mino.kraftwerk.features.SpecFeature
+import pink.mino.kraftwerk.scenarios.Scenario
+import pink.mino.kraftwerk.utils.Chat
+
+
+class WeakestLinkLogic : BukkitRunnable() {
+    var timer = 600
+    override fun run() {
+        if (timer == 0) {
+            timer = 600
+            if (WeakestLinkScenario().isAllSameHealth()) {
+                Bukkit.broadcastMessage(Chat.colored("${Chat.prefix} &fEveryone&7 was spared for now."))
+            } else {
+                val player = WeakestLinkScenario().getLowestHealth()
+                player.damage(999999.9)
+                Bukkit.broadcastMessage(Chat.colored("${Chat.prefix} &f${player.name}&7 was the &eWeakest Link&7!"))
+            }
+        }
+        timer -= 1
+    }
+}
+
+class WeakestLinkScenario : Scenario(
+    "Weakest Link",
+    "Every 10 minutes, the person with the least health will be killed.",
+    "weakestlink",
+    Material.IRON_BARDING
+) {
+    private val byHealth: Ordering<Player?> = object : Ordering<Player?>() {
+        override fun compare(p0: Player?, p1: Player?): Int {
+            return p0!!.health.compareTo(p1!!.health)
+        }
+    }
+
+    fun getLowestHealth() : Player {
+        val players = arrayListOf<Player>()
+        for (player in Bukkit.getOnlinePlayers()) {
+            if (!SpecFeature.instance.getSpecs().contains(player.name)) {
+                players.add(player)
+            }
+        }
+        return byHealth.min(players)
+    }
+
+    fun isAllSameHealth() : Boolean {
+        val players = arrayListOf<Player>()
+        for (player in Bukkit.getOnlinePlayers()) {
+            if (!SpecFeature.instance.getSpecs().contains(player.name)) {
+                players.add(player)
+            }
+        }
+        var last: Player? = null
+        for (player in players) {
+            if (last != null && last.health != player.health) {
+                return false
+            }
+            last = player
+        }
+        return true
+    }
+
+    override fun onStart() {
+        WeakestLinkLogic().runTaskTimer(JavaPlugin.getPlugin(Kraftwerk::class.java), 0L, 20L)
+    }
+}
