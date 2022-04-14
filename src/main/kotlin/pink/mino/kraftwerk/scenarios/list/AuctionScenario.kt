b451f5b17f4c4745942aa539226badda8620295a
Juan <jpichardo369@gmail.com>
2022-04-14 00:18:02 -0400

    Get started on Auctions
    Signed-off-by: Juan <jpichardo369@gmail.com>

---
+++ src/main/kotlin/pink/mino/kraftwerk/scenarios/list/AuctionScenario.kt
@@ -0,0 +1,11 @@
+package pink.mino.kraftwerk.scenarios.list
+
+import org.bukkit.Material
+import pink.mino.kraftwerk.scenarios.Scenario
+
+class AuctionScenario : Scenario(
+    "Auction",
+    "Teammates are sold sometime after the game opens.",
+    "auction",
+    Material.GOLD_BLOCK
+)
