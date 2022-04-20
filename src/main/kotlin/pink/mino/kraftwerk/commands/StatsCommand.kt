73f331b9960ee0eec65d974603192612f0f1f7fc
Juan <jpichardo369@gmail.com>
2022-04-20 07:18:18 -0400

    Fix /stats for offline players.
    Signed-off-by: Juan <jpichardo369@gmail.com>

--- src/main/kotlin/pink/mino/kraftwerk/commands/StatsCommand.kt
+++ src/main/kotlin/pink/mino/kraftwerk/commands/StatsCommand.kt
@@ -1,14 +1,14 @@
 package pink.mino.kraftwerk.commands
 
+import me.lucko.helper.promise.Promise
 import org.bukkit.Bukkit
 import org.bukkit.ChatColor
 import org.bukkit.Material
+import org.bukkit.OfflinePlayer
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
-import org.bukkit.inventory.ItemFlag
-import org.bukkit.inventory.ItemStack
 import pink.mino.kraftwerk.utils.Chat
 import pink.mino.kraftwerk.utils.GuiBuilder
 import pink.mino.kraftwerk.utils.ItemBuilder
@@ -26,119 +26,74 @@ class StatsCommand : CommandExecutor {
             sender.sendMessage("You can't use this command as you technically aren't a player.")
             return false
         }
-        val gui = GuiBuilder().rows(3).name(ChatColor.translateAlternateColorCodes('&', "&4Stats"))
-        if (args.isEmpty()) {
-            val player = StatsHandler.getStatsPlayer(sender)
-            val ores = ItemStack(Material.DIAMOND_ORE)
-            val oreMeta = ores.itemMeta
-            oreMeta.displayName = Chat.colored("&4Ores")
-            oreMeta.lore = listOf(
-                Chat.colored(Chat.guiLine),
-                "",
-                Chat.colored("&7Diamonds Mined ${Chat.dash} &c${player.diamondsMined}"),
-                Chat.colored("&7Gold Mined ${Chat.dash} &c${player.goldMined}"),
-                Chat.colored("&7Iron Mined ${Chat.dash} &c${player.diamondsMined}"),
-                "",
-                Chat.colored(Chat.guiLine)
-            )
-            ores.itemMeta = oreMeta
-
-            val general = ItemStack(Material.DIAMOND_SWORD)
-            val generalMeta = general.itemMeta
-            generalMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES)
-            generalMeta.displayName = Chat.colored("&4General")
-            generalMeta.lore = listOf(
-                Chat.colored(Chat.guiLine),
-                "",
-                Chat.colored("&7Kills ${Chat.dash} &c${player.kills}"),
-                Chat.colored("&7Deaths ${Chat.dash} &c${player.deaths}"),
-                Chat.colored("&7Wins ${Chat.dash} &c${player.wins}"),
-                Chat.colored("&7KDR ${Chat.dash} &c${round((player.kills.toDouble() / player.deaths.toDouble()) / 100.0)}"),
-                Chat.colored("&7Games Played ${Chat.dash} &c${player.gamesPlayed}"),
-                "",
-                Chat.colored(Chat.guiLine)
-            )
-            val misc = ItemBuilder(Material.WATCH)
-                .name("&4Misc.")
-                .addLore(Chat.guiLine)
-                .addLore(" ")
-                .addLore("&7Times Enchanted ${Chat.dash} &c${player.timesEnchanted}")
-                .addLore("&7Times Crafted  ${Chat.dash} &c${player.timesCrafted}")
-                .addLore("&7Gapples Eaten ${Chat.dash} &c${player.gapplesEaten}")
-                .addLore(" ")
-                .addLore(Chat.guiLine)
-                .make()
-            general.itemMeta = generalMeta
-
-            gui.item(12, general).onClick runnable@ {
-                it.isCancelled = true
-            }
-            gui.item(13, misc).onClick runnable@ {
-                it.isCancelled = true
-            }
-            gui.item(14, ores).onClick runnable@ {
-                it.isCancelled = true
-            }
-            sender.openInventory(gui.make())
+        val gui = GuiBuilder().rows(1).name(ChatColor.translateAlternateColorCodes('&', "&4Stats"))
+        val target: OfflinePlayer = if (args.isEmpty()) {
+            sender
         } else {
-            val target = Bukkit.getOfflinePlayer(args[0])
-            if (target == null) {
-                Chat.sendMessage(sender, "&cInvalid player!")
-                return false
-            }
-            val player = StatsHandler.getStatsPlayer(target)
-            val ores = ItemStack(Material.DIAMOND_ORE)
-            val oreMeta = ores.itemMeta
-            oreMeta.displayName = Chat.colored("&4Ores")
-            oreMeta.lore = listOf(
-                Chat.colored(Chat.guiLine),
-                "",
-                Chat.colored("&7Diamonds Mined ${Chat.dash} &c${player.diamondsMined}"),
-                Chat.colored("&7Gold Mined ${Chat.dash} &c${player.goldMined}"),
-                Chat.colored("&7Iron Mined ${Chat.dash} &c${player.ironMined}"),
-                "",
-                Chat.colored(Chat.guiLine)
-            )
-            ores.itemMeta = oreMeta
-
-            val general = ItemStack(Material.DIAMOND_SWORD)
-            val generalMeta = general.itemMeta
-            generalMeta.addItemFlags(ItemFlag.HIDE_ATTRIBUTES)
-            generalMeta.displayName = Chat.colored("&4General")
-            generalMeta.lore = listOf(
-                Chat.colored(Chat.guiLine),
-                "",
-                Chat.colored("&7Kills ${Chat.dash} &c${player.kills}"),
-                Chat.colored("&7Deaths ${Chat.dash} &c${player.deaths}"),
-                Chat.colored("&7KDR ${Chat.dash} &c${round((player.kills.toDouble() / player.deaths.toDouble()) / 100.0)}"),
-                Chat.colored("&7Wins ${Chat.dash} &c${player.wins}"),
-                Chat.colored("&7Games Played ${Chat.dash} &c${player.gamesPlayed}"),
-                "",
-                Chat.colored(Chat.guiLine)
-            )
-            val misc = ItemBuilder(Material.WATCH)
-                .name("&4Misc.")
-                .addLore(Chat.guiLine)
-                .addLore(" ")
-                .addLore("&7Times Enchanted ${Chat.dash} &c${player.timesEnchanted}")
-                .addLore("&7Times Crafted  ${Chat.dash} &c${player.timesCrafted}")
-                .addLore("&7Gapples Eaten ${Chat.dash} &c${player.gapplesEaten}")
-                .addLore(" ")
-                .addLore(Chat.guiLine)
-                .make()
-            general.itemMeta = generalMeta
-
-            gui.item(12, general).onClick runnable@ {
-                it.isCancelled = true
-            }
-            gui.item(13, misc).onClick runnable@ {
-                it.isCancelled = true
-            }
-            gui.item(14, ores).onClick runnable@ {
-                it.isCancelled = true
-            }
-            sender.openInventory(gui.make())
+            Bukkit.getOfflinePlayer(args[0])
         }
+        Promise.start()
+            .thenRunSync runnable@ {
+                Chat.sendMessage(sender, "${Chat.prefix} &7Loading stats for &f${target.name}&7...")
+            }
+            .thenApplyAsync { StatsHandler.getStatsPlayer(target) }
+            .thenAcceptSync runnable@ { statsPlayer ->
+                val ores = ItemBuilder(Material.DIAMOND_ORE)
+                    .name("&4Ores")
+                    .addLore(Chat.guiLine)
+                    .addLore(" ")
+                    .addLore("&7Diamonds Mined ${Chat.dash} &c${statsPlayer.diamondsMined}")
+                    .addLore("&7Gold Mined ${Chat.dash} &c${statsPlayer.goldMined}")
+                    .addLore("&7Iron Mined ${Chat.dash} &c${statsPlayer.ironMined}")
+                    .addLore(" ")
+                    .addLore(Chat.guiLine)
+                    .make()
+
+                val general = ItemBuilder(Material.DIAMOND_SWORD)
+                    .name("&4General")
+                    .addLore(Chat.guiLine)
+                    .addLore(" ")
+                    .addLore("&7Kills ${Chat.dash} &c${statsPlayer.kills}")
+                    .addLore("&7Deaths ${Chat.dash} &c${statsPlayer.deaths}")
+                    .addLore("&7Wins ${Chat.dash} &c${statsPlayer.wins}")
+                    .addLore("&7KDR ${Chat.dash} &c${round((statsPlayer.kills.toDouble() / statsPlayer.deaths.toDouble()))}")
+                    .addLore("&7Games Played ${Chat.dash} &c${statsPlayer.gamesPlayed}")
+                    .addLore(" ")
+                    .addLore(Chat.guiLine)
+                    .noAttributes()
+                    .make()
+
+                val misc = ItemBuilder(Material.WATCH)
+                    .name("&4Misc.")
+                    .addLore(Chat.guiLine)
+                    .addLore(" ")
+                    .addLore("&7Times Enchanted ${Chat.dash} &c${statsPlayer.timesEnchanted}")
+                    .addLore("&7Times Crafted  ${Chat.dash} &c${statsPlayer.timesCrafted}")
+                    .addLore("&7Gapples Eaten ${Chat.dash} &c${statsPlayer.gapplesEaten}")
+                    .addLore(" ")
+                    .addLore(Chat.guiLine)
+                    .make()
+                val skull = ItemBuilder(Material.SKULL_ITEM)
+                    .name("&f${target.name}")
+                    .addLore("&7The statistics for &f${target.name}&7.")
+                    .toSkull()
+                    .setOwner(target.name)
+                    .make()
+                gui.item(0, skull).onClick runnable@ {
+                    it.isCancelled = true
+                }
+                gui.item(3, general).onClick runnable@ {
+                    it.isCancelled = true
+                }
+                gui.item(4, misc).onClick runnable@ {
+                    it.isCancelled = true
+                }
+                gui.item(5, ores).onClick runnable@ {
+                    it.isCancelled = true
+                }
+                sender.openInventory(gui.make())
+            }
+
         return true
     }
 
