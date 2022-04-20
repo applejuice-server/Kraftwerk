c13d632910b53155568bef3e4f00a453d4f40cd8
Juan <jpichardo369@gmail.com>
2022-04-20 07:19:18 -0400

    Add active scenarios item & fix items
    Signed-off-by: Juan <jpichardo369@gmail.com>

--- src/main/kotlin/pink/mino/kraftwerk/features/SpawnFeature.kt
+++ src/main/kotlin/pink/mino/kraftwerk/features/SpawnFeature.kt
@@ -15,8 +15,9 @@ import org.bukkit.event.entity.FoodLevelChangeEvent
 import org.bukkit.event.inventory.InventoryClickEvent
 import org.bukkit.event.player.PlayerDropItemEvent
 import org.bukkit.event.player.PlayerInteractEvent
-import org.bukkit.inventory.ItemStack
+import org.bukkit.event.player.PlayerItemConsumeEvent
 import pink.mino.kraftwerk.utils.Chat
+import pink.mino.kraftwerk.utils.ItemBuilder
 
 class SpawnFeature : Listener {
     companion object {
@@ -38,32 +39,40 @@ class SpawnFeature : Listener {
         p.gameMode = GameMode.SURVIVAL
         p.exp = 0F
         p.level = 0
-        val arenaSword = ItemStack(Material.IRON_SWORD)
-        val arenaMeta = arenaSword.itemMeta
-        arenaMeta.displayName = Chat.colored("&cFFA Arena")
-        arenaMeta.lore = listOf(
-            Chat.colored("&7Right-click to send yourself into the FFA Arena.")
-        )
-        arenaSword.itemMeta = arenaMeta
+        val arenaSword = ItemBuilder(Material.IRON_SWORD)
+            .name("&cFFA Arena &7(Right Click)")
+            .addLore("&7Right-click to send yourself into the FFA Arena.")
+            .noAttributes()
+            .make()
         p.inventory.setItem(4, arenaSword)
-        val stats = ItemStack(Material.EMERALD)
-        val statsMeta = stats.itemMeta
-        statsMeta.displayName = Chat.colored("&cStats")
-        statsMeta.lore = listOf(
-            Chat.colored("&7Right-click to send yourself into the FFA Arena.")
-        )
-        stats.itemMeta = statsMeta
+
+        val stats = ItemBuilder(Material.WRITTEN_BOOK)
+            .name("&cView Stats &7(Right Click)")
+            .addLore("&7Right-click to view your stats.")
+            .make()
         p.inventory.setItem(5, stats)
-        val config = ItemStack(Material.ENCHANTED_BOOK)
-        val configMeta = config.itemMeta
-        configMeta.displayName = Chat.colored("&cUHC Configuration")
-        configMeta.lore = listOf(
-            Chat.colored("&7Right-click to view the UHC config.")
-        )
-        config.itemMeta = configMeta
+
+        val config = ItemBuilder(Material.GOLDEN_APPLE)
+            .name("&cUHC Configuration &7(Right Click)")
+            .addLore("&7Right-click to view the UHC Configuration.")
+            .make()
         p.inventory.setItem(3, config)
+
+        val scenarios = ItemBuilder(Material.EMERALD)
+            .name("&cActive Scenarios &7(Right Click)")
+            .addLore("&7Right-click to view active scenarios.")
+            .make()
+        p.inventory.setItem(0, scenarios)
+
         val location = Location(Bukkit.getWorld("Spawn"), -221.5, 95.0, -140.5)
         p.teleport(location)
+    }
+
+    @EventHandler
+    fun onPlayerConsume(e: PlayerItemConsumeEvent) {
+        if (e.player.world.name == "Spawn") {
+            e.isCancelled = true
+        }
     }
 
     @EventHandler
@@ -71,14 +80,21 @@ class SpawnFeature : Listener {
         if (e.player.world.name == "Spawn") {
             if (e.item !== null) {
                 when (e.item.itemMeta.displayName) {
-                    Chat.colored("&cFFA Arena") -> {
+                    Chat.colored("&cFFA Arena &7(Right Click)") -> {
+                        e.isCancelled = true
                         Bukkit.dispatchCommand(e.player, "a")
                     }
-                    Chat.colored("&cStats") -> {
+                    Chat.colored("&cView Stats &7(Right Click)") -> {
+                        e.isCancelled = true
                         Bukkit.dispatchCommand(e.player, "stats")
                     }
-                    Chat.colored("&cUHC Configuration") -> {
+                    Chat.colored("&cUHC Configuration &7(Right Click)") -> {
+                        e.isCancelled = true
                         Bukkit.dispatchCommand(e.player, "uhc")
+                    }
+                    Chat.colored("&cActive Scenarios &7(Right Click)") -> {
+                        e.isCancelled = true
+                        Bukkit.dispatchCommand(e.player, "scen")
                     }
                 }
             }
