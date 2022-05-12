c3db1af2dd9f74fa6c76990963d8c906e92e4ff0
Juan <jpichardo369@gmail.com>
2022-05-12 01:32:27 -0400

    Add spark implementation & a new tab list.
    Signed-off-by: Juan <jpichardo369@gmail.com>

--- src/main/kotlin/pink/mino/kraftwerk/features/TabFeature.kt
+++ src/main/kotlin/pink/mino/kraftwerk/features/TabFeature.kt
@@ -1,20 +1,79 @@
 package pink.mino.kraftwerk.features
 
+import me.lucko.spark.api.statistic.StatisticWindow.TicksPerSecond
+import me.lucko.spark.api.statistic.types.DoubleStatistic
 import net.minecraft.server.v1_8_R3.IChatBaseComponent.ChatSerializer
 import net.minecraft.server.v1_8_R3.PacketPlayOutPlayerListHeaderFooter
+import org.bukkit.Bukkit
 import org.bukkit.craftbukkit.v1_8_R3.entity.CraftPlayer
 import org.bukkit.entity.Player
-import org.bukkit.event.EventHandler
-import org.bukkit.event.Listener
-import org.bukkit.event.player.PlayerJoinEvent
+import org.bukkit.plugin.java.JavaPlugin
+import org.bukkit.scheduler.BukkitRunnable
+import pink.mino.kraftwerk.Kraftwerk
+import pink.mino.kraftwerk.scenarios.ScenarioHandler
+import pink.mino.kraftwerk.utils.Chat
 
 
-class TabFeature : Listener {
+class TabFeature : BukkitRunnable() {
+    fun checkTps(tps: Double): String {
+        if (tps > 19.0) {
+            return "§a" + tps.toString()
+        } else if (tps < 18.0) {
+            return "§e" + tps.toString()
+        } else if (tps < 16.0) {
+            return "§c" + tps.toString()
+        } else if (tps < 10.0) {
+            return "§4" + tps.toString()
+        }
+        return "&8" + tps.toString()
+    }
+
+    fun checkPing(ping: Int): String {
+        return if (ping < 50) {
+            "§a" + ping.toString()
+        } else if (ping < 100) {
+            "§e" + ping.toString()
+        } else if (ping < 200) {
+            "§c" + ping.toString()
+        } else if (ping < 500) {
+            "§4" + ping.toString()
+        } else {
+            "§8" + ping.toString()
+        }
+    }
+
+    fun scenarioTextWrap(text: String, width: Int): ArrayList<String> {
+        val words = text.split(" ")
+        val lines = ArrayList<String>()
+        var currentLine = ""
+        for (word in words) {
+            if (currentLine.length + word.length + 1 > width) {
+                lines.add(Chat.colored("&f${currentLine}"))
+                currentLine = "&f$word "
+            } else {
+                currentLine += "&f$word "
+            }
+        }
+        lines.add(Chat.colored("&f${currentLine}"))
+        return lines
+    }
+
     fun sendTablist(p: Player) {
         val craftplayer = p as CraftPlayer
         val connection = craftplayer.handle.playerConnection
-        val header = ChatSerializer.a("{\"text\": \" \"}")
-        val footer = ChatSerializer.a("{\"text\": \" \"}")
+        val tps: DoubleStatistic<TicksPerSecond>? = JavaPlugin.getPlugin(Kraftwerk::class.java).spark.tps()
+        val tpsLast10Secs = tps!!.poll(TicksPerSecond.SECONDS_10)
+        val scenarios = ArrayList<String>()
+        for (scenario in ScenarioHandler.getActiveScenarios()) {
+            scenarios.add(scenario.name)
+        }
+        if (scenarios.isEmpty()) {
+            scenarios.add("Vanilla+")
+        }
+        val header = ChatSerializer.a("{\"text\": \"${Chat.colored("\n&capple&ajuice")}\n${Chat.colored("&7TPS: ${checkTps(
+            Math.round(tpsLast10Secs * 100.0) / 100.0
+        )} &8| &7Ping: &f${checkPing(craftplayer.handle.ping)}")}ms\n\"}")
+        val footer = ChatSerializer.a("{\"text\": \"\n${Chat.colored(" &7Game: &f${SettingsFeature.instance.data!!.getString("game.host")}'s ${SettingsFeature.instance.data!!.getString("matchpost.team")} \n&7Scenarios: &f${scenarioTextWrap(scenarios.joinToString(", "), 40).joinToString("\n")} ")}\n\"}")
         val packet = PacketPlayOutPlayerListHeaderFooter()
         try {
             val headerField = packet.javaClass.getDeclaredField("a")
@@ -31,8 +90,9 @@ class TabFeature : Listener {
         connection.sendPacket(packet)
     }
 
-    @EventHandler
-    fun onPlayerJoin(e: PlayerJoinEvent) {
-        sendTablist(e.player)
+    override fun run() {
+        for (p in Bukkit.getOnlinePlayers()) {
+            sendTablist(p)
+        }
     }
 }
