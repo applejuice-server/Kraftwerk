6ee0035e04249c960df10a191e4b92c58ecaa6f0
Juan <jpichardo369@gmail.com>
2022-05-18 16:47:23 -0400

    Fix `Rewarding Longshots` not giving prizes.
    Signed-off-by: Juan <jpichardo369@gmail.com>

--- src/main/kotlin/pink/mino/kraftwerk/scenarios/list/RewardingLongshotsScenario.kt
+++ src/main/kotlin/pink/mino/kraftwerk/scenarios/list/RewardingLongshotsScenario.kt
@@ -7,6 +7,7 @@ import org.bukkit.entity.EntityType
 import org.bukkit.entity.Player
 import org.bukkit.event.EventHandler
 import org.bukkit.event.entity.EntityDamageByEntityEvent
+import org.bukkit.inventory.ItemStack
 import pink.mino.kraftwerk.scenarios.Scenario
 import pink.mino.kraftwerk.utils.Chat
 import pink.mino.kraftwerk.utils.GameState
@@ -27,35 +28,31 @@ class RewardingLongshotsScenario : Scenario(
         if (GameState.currentState != GameState.INGAME) return
         if (e.damager.type == EntityType.ARROW && ((e.damager as Arrow).shooter) is Player && e.entity.type == EntityType.PLAYER) {
             val distance = (e.damager as Arrow).location.distance((((e.damager as Arrow).shooter) as Player).location)
-            val prizes = ArrayList<Material>()
+            val prizes = ArrayList<ItemStack>()
             var broadcast = false
             if (distance > 30 && distance < 50) {
                 broadcast = true
-                prizes.add(Material.IRON_INGOT)
+                prizes.add(ItemStack(Material.IRON_INGOT))
             } else if (distance > 50 && distance < 100) {
                 broadcast = true
-                prizes.add(Material.IRON_INGOT)
-                prizes.add(Material.GOLD_INGOT)
+                prizes.add(ItemStack(Material.IRON_INGOT))
+                prizes.add(ItemStack(Material.GOLD_INGOT))
             } else if (distance > 100 && distance < 200) {
                 broadcast = true
-                prizes.add(Material.IRON_INGOT)
-                prizes.add(Material.GOLD_INGOT)
-                prizes.add(Material.DIAMOND)
+                prizes.add(ItemStack(Material.IRON_INGOT))
+                prizes.add(ItemStack(Material.GOLD_INGOT))
+                prizes.add(ItemStack(Material.DIAMOND))
             } else if (distance > 200) {
                 broadcast = true
-                prizes.add(Material.IRON_INGOT)
-                prizes.add(Material.IRON_INGOT)
-                prizes.add(Material.GOLD_INGOT)
-                prizes.add(Material.GOLD_INGOT)
-                prizes.add(Material.GOLD_INGOT)
-                prizes.add(Material.DIAMOND)
-                prizes.add(Material.DIAMOND)
-                prizes.add(Material.DIAMOND)
-                prizes.add(Material.DIAMOND)
-                prizes.add(Material.DIAMOND)
+                prizes.add(ItemStack(Material.IRON_INGOT, 2))
+                prizes.add(ItemStack(Material.GOLD_INGOT, 3))
+                prizes.add(ItemStack(Material.DIAMOND, 5))
             }
             if (broadcast) Bukkit.broadcastMessage(Chat.colored("$prefix &7${PlayerUtils.getPrefix((((e.damager as Arrow).shooter) as Player))}${(((e.damager as Arrow).shooter) as Player).name}&7 hit &7${e.entity.name} from over &f${floor(distance)} &7blocks away!"))
-            if (prizes.isNotEmpty()) Chat.sendMessage((((e.damager as Arrow).shooter) as Player), prefix + "You got &a${prizes.size} &7prizes!")
+            if (prizes.isNotEmpty()) {
+                Chat.sendMessage((((e.damager as Arrow).shooter) as Player), prefix + "You got &a${prizes.size} &7prizes!")
+                PlayerUtils.bulkItems((((e.damager as Arrow).shooter) as Player), prizes)
+            }
         }
     }
 }
