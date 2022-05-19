c29ff77540a3792d69d8473fb17404ac70e858e2
Juan <jpichardo369@gmail.com>
2022-05-18 21:05:37 -0400

    Fix arena regeneration
    Signed-off-by: Juan <jpichardo369@gmail.com>

--- src/main/kotlin/pink/mino/kraftwerk/commands/RegenArenaCommand.kt
+++ src/main/kotlin/pink/mino/kraftwerk/commands/RegenArenaCommand.kt
@@ -1,11 +1,16 @@
 package pink.mino.kraftwerk.commands
 
-import org.bukkit.Bukkit
+import me.lucko.helper.utils.Log
+import org.bukkit.*
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
 import org.bukkit.entity.Player
+import org.bukkit.plugin.java.JavaPlugin
+import pink.mino.kraftwerk.Kraftwerk
 import pink.mino.kraftwerk.utils.Chat
+import java.nio.file.Files
+import java.nio.file.Path
 
 class RegenArenaCommand : CommandExecutor {
     override fun onCommand(
@@ -18,14 +23,43 @@ class RegenArenaCommand : CommandExecutor {
             Chat.sendMessage(sender, "&cOnly console senders can execute this command.")
             return false
         }
-        Bukkit.dispatchCommand(
-            Bukkit.getConsoleSender(),
-            "pregen Arena 100 overworld"
+        var world = Bukkit.getWorld("Arena")
+        if (world != null) {
+            Bukkit.getServer().unloadWorld(world.name, true)
+            for (file in Bukkit.getServer().worldContainer.listFiles()!!) {
+                if (file.name.lowercase() == world.name.lowercase()) {
+                    Files.walk(file.toPath()).sorted(Comparator.reverseOrder()).map(Path::toFile).forEach { it.delete() }
+                    file.delete()
+                    Log.info("Deleted world file for ${world.name}.")
+                }
+            }
+        }
+        val wc = WorldCreator("Arena")
+        wc.environment(World.Environment.NORMAL)
+        wc.type(WorldType.NORMAL)
+        world = wc.createWorld()
+        world.difficulty = Difficulty.HARD
+
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+            "wb shape rectangular"
         )
-        Bukkit.dispatchCommand(
-            Bukkit.getConsoleSender(),
-            "mvgamerule doMobSpawning false Arena"
+        Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+            "wb Arena setcorners 100 100 -100 -100"
         )
+
+        Bukkit.getScheduler().runTaskLater(JavaPlugin.getPlugin(Kraftwerk::class.java), {
+            val border = world.worldBorder
+            border.size = 100.toDouble() * 2
+            border.setCenter(0.0, 0.0)
+
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+                "wb Arena fill 200"
+            )
+            Bukkit.dispatchCommand(Bukkit.getConsoleSender(),
+                "wb fill confirm"
+            )
+        }, 5L)
+
         return true
     }
 
