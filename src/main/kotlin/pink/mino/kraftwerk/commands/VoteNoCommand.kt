bf04c45f553ab32f31893bc1bf7094b48a1ddd85
Marcus <marcus.moquin@hotmail.com>
2022-05-19 17:59:50 -0400

    Increased Vote timer to 20s and Big Text!

--- src/main/kotlin/pink/mino/kraftwerk/commands/VoteNoCommand.kt
+++ src/main/kotlin/pink/mino/kraftwerk/commands/VoteNoCommand.kt
@@ -3,8 +3,10 @@ package pink.mino.kraftwerk.commands
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
 import org.bukkit.plugin.java.JavaPlugin
 import pink.mino.kraftwerk.Kraftwerk
+import pink.mino.kraftwerk.features.SpecFeature
 import pink.mino.kraftwerk.utils.Chat
 
 class VoteNoCommand : CommandExecutor {
@@ -18,11 +20,19 @@ class VoteNoCommand : CommandExecutor {
             Chat.sendMessage(sender, "&cThere is no poll running at the moment.")
             return false
         }
-        if (JavaPlugin.getPlugin(Kraftwerk::class.java).vote!!.voted.contains(sender)) {
+        val player = (sender as Player)
+
+        if (SpecFeature.instance.isSpec(player)) {
+            Chat.sendMessage(sender, "&cYou cannot vote while spectating.")
+            return false
+        }
+
+        if (JavaPlugin.getPlugin(Kraftwerk::class.java).vote!!.voted.contains(player)) {
             Chat.sendMessage(sender, "&cYou already voted.")
             return false
         }
         JavaPlugin.getPlugin(Kraftwerk::class.java).vote!!.no += 1
+        JavaPlugin.getPlugin(Kraftwerk::class.java).vote!!.voted.add(player)
         Chat.sendMessage(sender, "${Chat.prefix} Successfully voted &cno&7 on the current poll.")
         return true
     }
