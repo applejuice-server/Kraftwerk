4b07f11f7f3b63efd5e5e66b5f49d892dd054548
Marcus <marcus.moquin@hotmail.com>
2022-05-21 12:47:14 -0400

    More Discord VC Support

--- src/main/kotlin/pink/mino/kraftwerk/discord/listeners/MemberLeaveVC.kt
+++ src/main/kotlin/pink/mino/kraftwerk/discord/listeners/MemberLeaveVC.kt
@@ -4,13 +4,17 @@ import net.dv8tion.jda.api.entities.AudioChannel
 import net.dv8tion.jda.api.entities.VoiceChannel
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
+import java.util.concurrent.TimeUnit
 
 class MemberLeaveVC : ListenerAdapter() {
 
     override fun onGuildVoiceLeave(event: GuildVoiceLeaveEvent) {
-        val vc: AudioChannel = event.channelLeft
+        val vc: VoiceChannel = event.channelLeft as VoiceChannel
+        event.guild.getTextChannelById(756953696038027425)!!.sendMessage("called leave").queue runnable@ {
+            it.delete().queueAfter(5, TimeUnit.SECONDS)
+        }
 
-        if (!vc.name.contains("'s Channel")) {
+        if (!vc.name.contains("'s Channel", true)) {
             return
         }
 
