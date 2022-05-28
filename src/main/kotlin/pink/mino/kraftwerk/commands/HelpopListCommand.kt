5f42f3814fc576f438e65af08b9a47fa966df158
Marcus <marcus.moquin@hotmail.com>
2022-05-28 16:04:04 -0400

    Added HelpOp List Command to view unanswered helpops

---
+++ src/main/kotlin/pink/mino/kraftwerk/commands/HelpopListCommand.kt
@@ -0,0 +1,51 @@
+package pink.mino.kraftwerk.commands
+
+import net.md_5.bungee.api.chat.ClickEvent
+import net.md_5.bungee.api.chat.TextComponent
+import org.bukkit.ChatColor
+import org.bukkit.command.Command
+import org.bukkit.command.CommandExecutor
+import org.bukkit.command.CommandSender
+import org.bukkit.entity.Player
+import pink.mino.kraftwerk.utils.Chat
+import pink.mino.kraftwerk.utils.HelpOp
+
+class HelpopListCommand : CommandExecutor {
+
+    override fun onCommand(
+        sender: CommandSender,
+        cmd: Command?,
+        label: String?,
+        args: Array<out String>?
+    ): Boolean {
+        if (sender is Player) {
+            if (!sender.hasPermission("uhc.staff.hl")) {
+                Chat.sendMessage(sender, "${Chat.prefix} ${ChatColor.RED}You don't have permission to use this command.")
+                return false
+            }
+        }
+        Chat.sendMessage(sender, "${Chat.prefix} &cUnanswered Helpops:")
+        val count = HelpOp.getHelpops()
+        if (count == 0) {
+            Chat.sendMessage(sender, "${Chat.prefix} &cThere are no unanswered Helpops.")
+            return false
+        }
+        for (i in 1..count) {
+            if (HelpOp.isHelpopAnswered(i)) {
+                continue
+            }
+            if (sender is Player) {
+                val text =
+                    TextComponent(Chat.colored(" ${Chat.dash} &8[&c#${i}&8] &f${HelpOp.helpop[i]?.name} ${Chat.dash}&7 ${HelpOp.helpopContent[i]}"))
+                text.clickEvent = ClickEvent(
+                    ClickEvent.Action.SUGGEST_COMMAND,
+                    "/hr ${i} "
+                )
+                sender.spigot().sendMessage(text)
+            } else {
+                sender.sendMessage(Chat.colored(" ${Chat.dash} &8[&c#${i}&8] &f${HelpOp.helpop[i]!!.name} ${Chat.dash}&7 ${HelpOp.helpopContent[i]}"))
+            }
+        }
+        return true
+    }
+}
