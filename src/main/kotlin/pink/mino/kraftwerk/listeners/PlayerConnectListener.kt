0247757be1d935b8afbb66822ff5220546c77013
Marcus <marcus.moquin@hotmail.com>
2022-06-16 06:24:22 -0400

    Added detail to whitelist on kick messages

--- src/main/kotlin/pink/mino/kraftwerk/listeners/PlayerConnectListener.kt
+++ src/main/kotlin/pink/mino/kraftwerk/listeners/PlayerConnectListener.kt
@@ -3,8 +3,12 @@ package pink.mino.kraftwerk.listeners
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.player.PlayerLoginEvent
+import org.bukkit.plugin.java.JavaPlugin
+import pink.mino.kraftwerk.Kraftwerk
+import pink.mino.kraftwerk.features.Events
 import pink.mino.kraftwerk.features.SettingsFeature
 import pink.mino.kraftwerk.utils.Chat
+import pink.mino.kraftwerk.utils.GameState
 
 class PlayerConnectListener : Listener {
     @EventHandler
@@ -13,7 +17,38 @@ class PlayerConnectListener : Listener {
         if (SettingsFeature.instance.data!!.getBoolean("whitelist.enabled")) {
             if (!SettingsFeature.instance.data!!.getList("whitelist.list").contains(player.name.lowercase())) {
                 if (!player.hasPermission("uhc.staff")) {
-                    e.disallow(PlayerLoginEvent.Result.KICK_WHITELIST, Chat.colored("&cYou are not allowed to join while the whitelist is on!\n\n&7Get pre-whitelisted on the discord @ &cdsc.gg/apple-juice&7!"))
+                    if (GameState.currentState == GameState.INGAME) {
+                        if (JavaPlugin.getPlugin(Kraftwerk::class.java).game == null) {
+                            e.disallow(
+                                PlayerLoginEvent.Result.KICK_WHITELIST,
+                                Chat.colored("&cYou are not allowed to join while the whitelist is on!\n&cThere is currently no game happening on this server.\n\n&7Get pre-whitelisted on the discord @ &cdsc.gg/apple-juice&7!")
+                            )
+                            return
+                        }
+                        if (JavaPlugin.getPlugin(Kraftwerk::class.java).game!!.currentEvent == Events.PVP || JavaPlugin.getPlugin(Kraftwerk::class.java).game!!.currentEvent == Events.MEETUP) {
+                            e.disallow(
+                                PlayerLoginEvent.Result.KICK_WHITELIST,
+                                Chat.colored("&cYou are not allowed to join while the whitelist is on!\n&cPvP is currently enabled and no more players will be able to join late!\n\n&7Get pre-whitelisted on the discord @ &cdsc.gg/apple-juice&7!")
+                            )
+                        } else {
+                            if (SettingsFeature.instance.data!!.getString("matchpost.team") == "Auctions" || SettingsFeature.instance.data!!.getString("matchpost.team").contains("Random")) {
+                                e.disallow(
+                                    PlayerLoginEvent.Result.KICK_WHITELIST,
+                                    Chat.colored("&cYou are not allowed to join while the whitelist is on!\n&cYou cannot late scatter in an Auction or a Random teams game!\n\n&7Get pre-whitelisted on the discord @ &cdsc.gg/apple-juice&7!")
+                                )
+                                return
+                            }
+                            e.disallow(
+                                PlayerLoginEvent.Result.KICK_WHITELIST,
+                                Chat.colored("&cYou are not allowed to join while the whitelist is on!\n&cPlease wait until the host accepts late scatters!\n\n&7Get pre-whitelisted on the discord @ &cdsc.gg/apple-juice&7!")
+                            )
+                        }
+                    } else {
+                        e.disallow(
+                            PlayerLoginEvent.Result.KICK_WHITELIST,
+                            Chat.colored("&cYou are not allowed to join while the whitelist is on!\n&cThere is currently no game happening on this server.\n\n&7Get pre-whitelisted on the discord @ &cdsc.gg/apple-juice&7!")
+                        )
+                    }
                 } else {
                     return
                 }
