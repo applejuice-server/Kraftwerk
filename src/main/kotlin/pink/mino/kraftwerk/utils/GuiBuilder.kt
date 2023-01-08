cd8f26dc2fa44090d127df99c5ea47d0ba6fdcfe
Juan <jpichardo369@gmail.com>
2023-01-08 15:24:09 -0500

    Fix players occasionally not being able to move items in their inventory
    Signed-off-by: Juan <jpichardo369@gmail.com>

--- src/main/kotlin/pink/mino/kraftwerk/utils/GuiBuilder.kt
+++ src/main/kotlin/pink/mino/kraftwerk/utils/GuiBuilder.kt
@@ -69,7 +69,6 @@ class GuiBuilder : Listener {
                             }
                         }
                     }
-                    e.isCancelled = true
                 }
             }
         }
