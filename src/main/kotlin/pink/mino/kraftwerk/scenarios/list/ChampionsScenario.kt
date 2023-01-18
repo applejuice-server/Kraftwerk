41b8c05b7ba3619ea1cc756f6114e99ce0e91044
Juan <jpichardo369@gmail.com>
2023-01-18 18:17:31 -0500

    Added Krenzinator & rewrote crafting recipes
    Signed-off-by: Juan <jpichardo369@gmail.com>

--- src/main/kotlin/pink/mino/kraftwerk/scenarios/list/ChampionsScenario.kt
+++ src/main/kotlin/pink/mino/kraftwerk/scenarios/list/ChampionsScenario.kt
@@ -64,20 +64,7 @@ class ChampionsScenario : Scenario(
     override fun onToggle(to: Boolean) {
         if (!to) {
             Bukkit.resetRecipes()
-            val mater = MaterialData(Material.SKULL_ITEM)
-            mater.data = 3.toByte()
-            val head = ItemStack(Material.GOLDEN_APPLE)
-            val meta: ItemMeta = head.itemMeta
-            meta.displayName = ChatColor.GOLD.toString() + "Golden Head"
-            meta.lore = listOf(
-                ChatColor.DARK_PURPLE.toString() + "Some say consuming the head of a",
-                ChatColor.DARK_PURPLE.toString() + "fallen foe strengthens the blood."
-            )
-            head.itemMeta = meta
-            val goldenHead: ShapedRecipe =
-                ShapedRecipe(head).shape("@@@", "@*@", "@@@").setIngredient('@', Material.GOLD_INGOT)
-                    .setIngredient('*', mater)
-            Bukkit.getServer().addRecipe(goldenHead)
+            JavaPlugin.getPlugin(Kraftwerk::class.java).addRecipes()
         } else {
             Bukkit.resetRecipes()
             val vorpalSword = ItemBuilder(Material.IRON_SWORD)
