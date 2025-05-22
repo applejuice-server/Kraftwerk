9764b186851121d0d353f3466aaf645cd8ce58e9
minotaa <jpichardo369@gmail.com>
2025-05-22 02:43:23 -0400

    Separate config.yml into `config.yml` and `data.yml`
    Signed-off-by: minotaa <jpichardo369@gmail.com>

--- src/main/kotlin/pink/mino/kraftwerk/commands/ApplyCommand.kt
+++ src/main/kotlin/pink/mino/kraftwerk/commands/ApplyCommand.kt
@@ -3,7 +3,7 @@ package pink.mino.kraftwerk.commands
 import org.bukkit.command.Command
 import org.bukkit.command.CommandExecutor
 import org.bukkit.command.CommandSender
-import pink.mino.kraftwerk.features.SettingsFeature
+import pink.mino.kraftwerk.features.ConfigFeature
 import pink.mino.kraftwerk.utils.Chat
 
 class ApplyCommand : CommandExecutor {
@@ -13,7 +13,7 @@ class ApplyCommand : CommandExecutor {
         label: String?,
         args: Array<String>
     ): Boolean {
-        sender.sendMessage(Chat.colored("${Chat.prefix} The staff application URL is: ${Chat.primaryColor}${if (SettingsFeature.instance.data!!.getString("config.chat.staffAppUrl") != null) SettingsFeature.instance.data!!.getString("config.chat.staffAppUrl") else "no staff app set in config tough tits"}"))
+        sender.sendMessage(Chat.colored("${Chat.prefix} The staff application URL is: ${Chat.secondaryColor}${if (ConfigFeature.instance.config!!.getString("chat.staffAppUrl") != null) ConfigFeature.instance.config!!.getString("chat.staffAppUrl") else "no staff app set in config tough tits"}"))
         return true
     }
 }
